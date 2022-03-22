f2bea933d3001c0227b886073dbea9f50815c4d2
Student 1 Account <student1@hpc258.tcnj.edu>
2022-03-22 00:28:40 -0400

    Add dashboard

---
+++ src/app/controllers/dashboard_controller.rb
@@ -0,0 +1,19 @@
+# Project name: Assignment 2: TAG
+# Description: This website will track donations and related information 
+# to allow nonprofits to better streamline their process of reallocating
+# donations to other distribution centers, who in turn give these 
+# donations to the homeless or those who are unable to afford them.
+# Filename: dashboard_controller.rb
+# Description: This file will add the dashboard with eventual functionality
+# Last modified on: 3/22/22
+
+class DashboardController < ApplicationController
+  def dash
+  end
+  def search_data
+  end
+  def add_data
+  end
+  def delete_data
+  end
+end
