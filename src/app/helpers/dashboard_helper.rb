4b579d4a3cd3f0391b161de6967659f336a2fb7b
Student 1 Account <student1@hpc258.tcnj.edu>
2022-03-22 00:26:34 -0400

    Add documentation for dashboard_helper

--- src/app/helpers/dashboard_helper.rb
+++ src/app/helpers/dashboard_helper.rb
@@ -1,2 +1,11 @@
+# Project name: Assignment 2: TAG
+# Description: This website will track donations and related information 
+# to allow nonprofits to better streamline their process of reallocating
+# donations to other distribution centers, who in turn give these 
+# donations to the homeless or those who are unable to afford them.
+# Filename: dashboard_helper.rb
+# Description: This file will add the dashboard with eventual functionality
+# Last modified on: 3/22/22
+
 module DashboardHelper
 end
