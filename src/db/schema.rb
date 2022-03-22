a06347aa704453193c50d5d88f3868b407af9d7a
Student 1 Account <student1@hpc258.tcnj.edu>
2022-03-22 02:30:04 -0400

    Add extension to support db

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_13_190602) do
 
+  # Need to enable this extension to support this database
+  enable_extension "plpgsql"
+
   create_table "app_configs", force: :cascade do |t|
     t.string "name"
     t.string "logo_url"
