a5bed29cbff9b7d139ac3fd28f67c6057f12b2c2
Student 1 Account <student1@hpc258.tcnj.edu>
2022-03-21 23:47:15 -0400

    Fix do loop

--- src/test/controllers/dashboard_controller_test.rb
+++ src/test/controllers/dashboard_controller_test.rb
@@ -10,7 +10,7 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get dash url" do
+  test "should get dash" do
     get dashboard_dash_url
     assert_response :success
   end
