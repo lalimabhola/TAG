9b8f308ec17d5b4716d1c45a4aa4f1da2b03274a
Student 1 Account <student1@hpc258.tcnj.edu>
2022-04-21 00:20:05 -0400

    Performs search by first name (not final)

--- src/app/controllers/searches_controller.rb
+++ src/app/controllers/searches_controller.rb
@@ -10,15 +10,30 @@
 class SearchesController < ApplicationController
 
     # listing possible search parameters
-    def index
-        # @donations = Donation.accessible_by(current_ability)
-        @donations = @donations.search(params[:query])
-    end
+    # def index
+    #     # @donations = Donation.accessible_by(current_ability)
+    #     @donations = @donations.search(params[:first_name]) unless not params.has_key? :first_name
+    # end
+
+    # :first_name, :last_name, :email, :donation_item, :donation_type, :donation_quantity
+
+    # def index
+    #     @donations = Donation.where(nil) # creates an anonymous scope
+    #     @donations = @donations.filter_by_first_name(params[:first_name]) if params[:first_name].present?
+    # end
   
+    def index
+        @donations = Donation.search(params[:first_name])
+    end
+
       # creating a new search
     def create
         respond_to do |format|
             format.html { render :index, query: params[:query] }
         end
     end 
+
+    def donation_params
+        params.require(:donation).permit(:first_name, :last_name, :email, :donation_item, :donation_type, :donation_quantity)
+    end
 end
--- src/app/models/donation.rb
+++ src/app/models/donation.rb
@@ -1,2 +1,17 @@
 class Donation < ApplicationRecord
+    belongs_to :donation
+
+    def self.search(search)
+        if search
+            donation = Donation.find_by(first_name: search)
+            if donation
+                self.where(donation_item: donation)
+            else
+                Donation.all
+            end
+        else
+            Donation.all
+        end
+    end
+    
 end
